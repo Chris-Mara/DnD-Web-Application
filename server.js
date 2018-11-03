f5da35de4283d08e604acfa226d0a517aa0c2c8d
terminalfuzz <terminal.fuzz@gmail.com>
2018-11-03 15:19:54 -0400

    Server POST Handling

--- server.js
+++ server.js
@@ -4,6 +4,20 @@ var app = express();
 
 var PORT_NUM = 5000;
 
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.json());
+
+app.get("/Login", function(request, response) {
+    response.sendFile("Public/Login/index.html");
+});
+
+app.post("/Login", function(request, response) {
+    var username = request.body.username;
+    var password = request.body.password;
+    console.log("Username is: " + username + ". Password is: " + password);
+    response.end("yes");
+});
+
 app.listen(PORT_NUM, function() {
     console.log("Listening on port " + PORT_NUM);
 });
