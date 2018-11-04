ad25b5e146cd6c7f6a5a26db20debf3924aa1ad5
JLieter <JLieter@gmail.com>
2018-11-04 02:19:25 -0500

    Added Sessions page

---
+++ public/Session/script.js
@@ -0,0 +1,9 @@
+function logout() {
+	window.location.href="../Login/index.html";
+	//Change value in table to offline (1 to 0?)
+}
+
+function chatroom() {
+	window.location.href="./Chatroom/index.html";
+	// Create the correct chatroom dynamically
+}
